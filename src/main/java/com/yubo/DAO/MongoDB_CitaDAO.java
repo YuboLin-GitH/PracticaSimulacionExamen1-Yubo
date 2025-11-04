44d907f232e46961f05d936aed444af6d21af140
yubo <yubo.lin@educa.jcyl.es>
2025-11-04 23:53:20 +0100

    cambiar algo

---
+++ src/main/java/com/yubo/DAO/MongoDB_CitaDAO.java
@@ -0,0 +1,45 @@
+package com.yubo.DAO;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.yubo.Connection.MongoDB_ConnectionDB;
+import com.yubo.domain.Cita;
+import com.yubo.util.AlertUtils;
+import org.bson.Document;
+
+/**
+ * ClassName: MongoDB_CitaDAO
+ * Package: com.yubo.DAO
+ * Description:
+ *
+ * @Author linlin
+ * @Create 04/11/2025 23:41
+ * @Version 1.0
+ */
+public class MongoDB_CitaDAO {
+    MongoClient mongoClient;
+    MongoDatabase mongoDatabase; // BASE DE DATOS DE MONGO
+    MongoCollection<Document> collection;
+
+    public MongoDB_CitaDAO() {
+        mongoClient = MongoDB_ConnectionDB.conectar();
+        mongoDatabase = mongoClient.getDatabase("ExamenCitasMedicas");
+        collection = mongoDatabase.getCollection("CitasMedicas");
+    }
+    public boolean insertCita(Cita cita) {
+        Document doc = new Document(); // DOCUMENTO BSON QUE SE INSERTARA EN LA BD
+        try {
+            doc.append("idCita", cita.getIdCita())
+                    .append("fechaCita", cita.getFechaCita())
+                    .append("idEspecialidad", cita.getEspecialidad().getIdEspecialidad())
+                    .append("idPaciente", cita.getPaciente().getIdPaciente());
+
+            collection.insertOne(doc); // INSERTAR EL DOCUMENTO EN LA COLECCION
+            return true;
+        } catch (Exception e) {
+            AlertUtils.mostrarError("Error al conectar a la base de datos: " + e.getMessage());
+        }
+        return false;
+    }
+}
---
+++ src/main/java/com/yubo/DAO/MongoDB_CitaInterface.java
@@ -0,0 +1,13 @@
+package com.yubo.DAO;
+
+/**
+ * ClassName: MongoDB_CitaInterface
+ * Package: com.yubo.DAO
+ * Description:
+ *
+ * @Author linlin
+ * @Create 04/11/2025 23:53
+ * @Version 1.0
+ */
+public interface MongoDB_CitaInterface {
+}
